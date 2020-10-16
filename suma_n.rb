0f9b3d95278b002aa7caed22c4a54c6f894336de
alfredopl <aalfredoplz@gmail.com>
2020-10-16 11:03:09 -0300

    Completed

--- suma_n.rb
+++ suma_n.rb
@@ -1,1 +1,9 @@
-puts "Por favor ingresar un numero hasta donde desea hacer un conteo por pantalla"
+n = ARGV[0].to_i
+acum = 0
+i=1
+while i <= n 
+        acum+=i
+        puts acum
+        i+=1
+end        
+puts"la suma total es #{acum}"
